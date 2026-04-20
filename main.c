b70c2bed8036e357e6ff756d16c1583a46c04231
SANTOOS00 <mohamederraiss@icloud.com>
2026-04-20 17:10:47 +0100

    fix: Improved stability by fixing incorrect pointer handling and thread initialization issues in manager workflow.

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: santoos <santoos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 02:45:44 by username          #+#    #+#             */
-/*   Updated: 2026/04/20 15:11:21 by santoos          ###   ########.fr       */
+/*   Updated: 2026/04/20 17:08:13 by moerrais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,14 @@ t_stack	*push_stack(void *arg)
 
 	head_node = NULL;
 	if (!arg)
-		return (head_node);
+	return (head_node);
 	new_node = malloc(sizeof(t_stack));
 	if (!new_node)
-		return (NULL);
+	return (NULL);
 	new_node->arry_key_coders = arg;
 	new_node->next = head_node;
 	head_node = new_node;
 	return (head_node);
-}
-
-t_coder	**create_coders(t_config config)
-{
-	static t_coder	**coders;
-	int				i;
-
-	i = 0;
-	if (coders == NULL)
-	{
-		coders = malloc(sizeof(t_coder **) * config.number_of_coders);
-		while (i < config.number_of_coders)
-			coders[i++] = malloc(sizeof(t_coder));
-	}
-	return (coders);
 }
 
 t_dongle	*create_dongles(t_config config)
@@ -52,8 +37,57 @@ t_dongle	*create_dongles(t_config config)
 		dongle = malloc(sizeof(t_dongle) * config.number_of_coders);
 	return (dongle);
 }
+t_coder	**create_coders(t_config config)
+{
+	static t_coder	**coders;
+	int				i;
+	
+	i = 0;
+	if (coders == NULL)
+	{
+		coders = malloc(sizeof(t_coder **) * config.number_of_coders);
+		while (i < config.number_of_coders)
+			coders[i++] = malloc(sizeof(t_coder));
+		// coders[i] = NULL;
+	}
+	return (coders);
+}
 
-t_coder	**join_dongle(t_config config)
+
+
+void *test(void *arg)
+{
+	t_coder *coder;
+
+	coder = (t_coder *)arg;
+	printf("%p %d\n", coder, coder->id);
+	return NULL;
+}
+void *create_threads(t_config config)
+{
+	t_coder **coders;
+	int i;
+	
+	coders = create_coders(config);
+	i = 0;
+	while(i < config.number_of_coders)
+	{
+		pthread_create(&coders[i]->thread, NULL, test, coders[i]);
+		printf("%p %d\n", coders[i], coders[i]->id);
+		i++;
+	}
+	i = 0;
+	while(config.number_of_coders > i)
+	{
+		pthread_join(coders[i]->thread, NULL);
+		i++;
+	}	
+
+	return coders;
+}
+
+
+int join_coders(t_config config)
 {
 	t_dongle	*dongles;
 	t_coder		**coders;
@@ -67,19 +101,19 @@ t_coder	**join_dongle(t_config config)
 		coders[i]->left = &dongles[i];
 		coders[i]->right = &dongles[(i + 1) % config.number_of_coders];
 		coders[i]->id = i;
+		printf("%p %d\n", coders[i], coders[i]->id);
 		i++;
 	}
-	return (coders);
+	return 0;
 }
 
-void	*manger_threads(t_config config)
+int	manger_threads(t_config config)
 {
-	t_coder	**coders;
-
-	coders = join_coders(config);
-	if (!coders)
-		return (NULL);
-	create_threads(coders);
+	if (join_coders(config))
+	return (1);
+	if (!create_threads(config))
+	return (1);
+	return (0);
 }
 
 int	main(int ac, char **av)
@@ -87,8 +121,9 @@ int	main(int ac, char **av)
 	t_config	config;
 
 	config = parse_args(ac, av);
-	if (create_threads(config) == NULL)
+	if (manger_threads(config) == 1)
 		return (1);
+	return (0);
 }
 // int	create_coders(t_config config)
 // {
--- parse_args.c
+++ parse_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: santoos <santoos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 13:03:45 by username          #+#    #+#             */
-/*   Updated: 2026/04/20 13:18:34 by santoos          ###   ########.fr       */
+/*   Updated: 2026/04/20 17:08:41 by moerrais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_config	parse_args(int ac, char **av)
 {
 	t_config	config;
 
-	config.number_of_coders = 10;
+	config.number_of_coders = 5;
 	config.time_to_compile = 200;
 	config.time_to_debug = 200;
 	config.time_to_refactor = 200;
